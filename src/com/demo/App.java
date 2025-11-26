5fda12f2cb13afaa305f278be054b89c1c0cde32
safaNj <najjarsafa57@gmail.com>
2025-11-26 01:45:35 +0100

    Modification de App

--- src/com/demo/App.java
+++ src/com/demo/App.java
@@ -2,16 +2,8 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        int result = calculer();
-        
-    }
-    
-    public static int calculer() {
-        return 5 + 3;  
-    }
+        int result = 5 / 0; // bug potentiel : division par zéro
 
-
+        int unusedVariable = 10; // variable non utilisée
+    }
 }
- public static int calculer() {
-        return 5 + 3;  
-    }
